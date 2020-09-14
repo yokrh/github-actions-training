5b4339966dfeb96d9c4501275ab6913e2b65da04
yokrh <y.oka.gml@gmail.com>
2020-09-14 17:12:19 +0900

    read other file in python script

--- .github/workflows/python-workflow/my_script.py
+++ .github/workflows/python-workflow/my_script.py
@@ -1,4 +1,13 @@
 import sys
+import os
 
 print("hello github action")
 print(sys.version)
+print(os.getcwd())
+
+# path = "../../../doc/sandbox/a.yml"
+path = "doc/sandbox/a.yml"
+
+with open(path) as f:
+  s = f.read()
+  print(s)
